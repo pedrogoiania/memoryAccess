64e4a4484b013e8c9bd18d8845dab1bcc20c97ba
pedrogyn <glad.pedro@gmail.com>
2017-03-01 23:34:44 -0300

    app project start first commit

---
+++ app/src/main/java/topicos_avancados/com/br/memoryaccess/MainActivity.java
@@ -0,0 +1,15 @@
+package topicos_avancados.com.br.memoryaccess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int valor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
